544f2a246a93b6be2ff1b104637b3afee4a5764c
Aashirwad Singh <143715246+Aashirwad0048@users.noreply.github.com>
2025-07-10 14:34:43 +0530

    first commit

--- myapp/src/config/api.js
+++ myapp/src/config/api.js
@@ -1,7 +1,7 @@
 // API Configuration
 const API_CONFIG = {
   // Base URL for API calls
-  BASE_URL: 'https://hms-storing.onrender.com',
+  BASE_URL: 'https://hms-storing-gb32.vercel.app/',
   
   // API Endpoints
   ENDPOINTS: {
